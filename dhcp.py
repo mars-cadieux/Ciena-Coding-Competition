0b0a07b9dab6e0665929487859ff489f60d2b5c0
Mars Cadieux <cadieuxmars@gmail.com>
2024-02-01 19:35:00 -0500

    main and menu finished

--- dhcp.py
+++ dhcp.py
@@ -1,9 +1,9 @@
 assignedIPAddresses = {}
-
-
+retiredIPAddresses = []
+ipGenerator = [0,0,0,0]
+currIndex = 3
 
 import sys
-import re
 import time
 
 def ask(inputIP:str) -> str:
@@ -11,7 +11,7 @@ def ask(inputIP:str) -> str:
 		if inputIP in assignedIPAddresses:
 			print("This IP address is already assigned.")
 		else:
-			assignedIPAddresses[inputIP] = "assigned"
+			assignedIPAddresses[inputIP] = time.time()
 			print("Offer " + inputIP)
 	else:
 		print("The inputted string does not resemble an IP address.")
@@ -29,7 +29,18 @@ def release(inputIP:str) -> str:
 	if(checkValidIP(inputIP)):
 		if inputIP in assignedIPAddresses:
 			assignedIPAddresses.pop(inputIP)
+			retiredIPAddresses.append(inputIP)
 			print("RELEASED for " + inputIP)
+		else:
+			print("This IP address is not assigned.")
+	else:
+		print("The inputted string does not resemble an IP address.")
+
+def renew(inputIP:str) -> str:
+	if(checkValidIP(inputIP)):
+		if inputIP in assignedIPAddresses:
+			assignedIPAddresses[inputIP] = time.time()
+			print("RENEWED for " + inputIP)
 		else:
 			print("This IP address is not assigned.")
 	else:
@@ -43,6 +54,41 @@ def checkValidIP(inputIP: str) -> bool:
 	for segment in IPSegments:
 		if not (segment.isnumeric()):
 			return False
-		if segment < 0 or segment > 255:
+		if int(segment) < 0 or int(segment) > 255:
 			return False
 	return True
+
+def generateIP() -> str:
+	if len(retiredIPAddresses) > 0:
+		newIP = retiredIPAddresses[0]
+		retiredIPAddresses.remove(newIP)
+		return newIP
+	else:
+		if ipGenerator[currIndex] > 255:
+			ipGenerator[currIndex] = 0
+			currIndex -= 1
+			ipGenerator[currIndex] 
+
+
+def main():
+	print("Welcome to the DHCP server! Type in a command to get started. Type 0 to exit.\n")
+	userInput = input("> ")
+	while(userInput != "0"):
+		if userInput.startswith("ASK"):
+			ask(userInput[4:])
+			userInput = input("> ")
+		elif userInput.startswith("RENEW"):
+			renew(userInput[6:])
+			userInput = input("> ")
+		elif userInput.startswith("RELEASE"):
+			release(userInput[8:])
+			userInput = input("> ")
+		elif userInput.startswith("STATUS"):
+			status(userInput[7:])
+			userInput = input("> ")
+
+
+
+
+if __name__ == "__main__":
+    main()
